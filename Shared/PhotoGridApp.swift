359f27764912139456a1105fac7ecfa5afb65281
Lucas Silveira <lucas.silveira@ilia.digital (SIS-7588)>
2021-02-24 17:16:42 -0300

    Initial Commit

---
+++ Shared/PhotoGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoGridApp.swift
+//  Shared
+//
+//  Created by Lucas Silveira on 24/02/21.
+//
+
+import SwiftUI
+
+@main
+struct PhotoGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
